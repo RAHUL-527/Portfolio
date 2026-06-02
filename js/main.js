bd9248fd17c055ef521889f3bafeea3e8d23edcc
RAHUL R <148835501+RAHUL-527@users.noreply.github.com>
2026-06-02 17:38:42 +0530

    main.js

--- js/main.js
+++ js/main.js
@@ -9,3 +9,46 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+
+/* *********************************************************************** */
+                    // EMAIL CONTACT FORM
+/* *********************************************************************** */
+
+const form = document.querySelector(".contact-form");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+    result.innerHTML = "Sending...";
+
+    const formData = new FormData(form);
+
+    try {
+        const response = await fetch(
+            "https://api.web3forms.com/submit",
+            {
+                method: "POST",
+                body: formData
+            }
+        );
+
+        const data = await response.json();
+
+        if (data.success) {
+            result.innerHTML =
+                "✅ Message sent successfully!";
+            result.style.color = "#38bdf8";
+
+            form.reset();
+        } else {
+            result.innerHTML =
+                "❌ Failed to send message.";
+            result.style.color = "#ff4d4d";
+        }
+    } catch (error) {
+        result.innerHTML =
+            "❌ Something went wrong.";
+        result.style.color = "#ff4d4d";
+    }
+});
